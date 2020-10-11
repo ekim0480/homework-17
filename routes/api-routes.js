e179f2bc6b166b7b0b3f871a35bcfeea6043380b
EK <ekim0480@gmail.com>
2020-10-11 18:31:10 -0500

    final product

---
+++ routes/api-routes.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+const db = require("../models");
+
+// get all workouts
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// get workout within range
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// create workout
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// add exercise to workout
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.update({ _id: req.params.id }, { $push: { exercises: req.body } })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+// dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+// define port
+const PORT = process.env.PORT || 3000;
+
+// initialize app with express
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+// creating connection to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => console.log(`Connected on PORT ${PORT}`));
